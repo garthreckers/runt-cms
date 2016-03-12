747a7f70a30ff98b2e0989cd576f8760969763c5
Garth Reckers <garthreckers@Garths-MacBook-Pro.local>
2016-03-11 20:32:44 -0800

    early admin setup

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
+
+app.debug = True
 
 @app.route("/")
 def home():
 	return "Homepage"
+
+@app.route("/admin")
+def admin():
+	return render_template('admin/main-admin.html')
 
 if __name__ == "__main__":
 	app.run()
