f925aebd07b2db78e18841dbe497c600803ea1b6
Garth Reckers <garthreckers@Garths-MacBook-Pro.local>
2016-03-12 18:29:01 -0800

    lots of structuring changes

--- app.py
+++ app.py
@@ -1,15 +1,10 @@
-from flask import Flask, render_template
-app = Flask(__name__)
+from runt.trigger import trigger
+from flask import render_template
 
-app.debug = True
-
-@app.route("/")
+@trigger.route("/")
 def home():
-	return "Homepage"
+	return render_template('placeholder.html')
 
-@app.route("/admin")
-def admin():
-	return render_template('admin/main-admin.html')
 
 if __name__ == "__main__":
-	app.run()
+	trigger.run()
---
+++ runt/trigger.py
@@ -0,0 +1,26 @@
+"""
+
+Builds the basics for Runt
+
+"""
+import jinja2
+from flask import Flask, render_template
+from runt import trigger
+
+trigger = Flask(__name__)
+
+trigger.debug = True
+
+theme_loader = jinja2.ChoiceLoader([
+	    trigger.jinja_loader,	
+		jinja2.FileSystemLoader([
+				'runt/themes',
+				'runt/admin/templates'
+			])
+	])
+trigger.jinja_loader = theme_loader
+
+
+@trigger.route("/admin")
+def admin():
+	return render_template('admin-main.html')
