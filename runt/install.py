2e50b77cc6418d3dc94358547d724e56fb64e425
root <garthreckers@yahoo.com>
2016-03-13 15:17:29 -0400

    changes were made

--- runt/install.py
+++ runt/install.py
@@ -1,2 +1,17 @@
-from flask.ext.mysql import MySQL
-from trigger import trigger
+#!/bin/env/Python3
+from flask.ext.mysqldb import MySQL
+from . import config
+from .trigger import trigger
+
+
+def build():
+	query = "CREATE TABLE settings (\
+		id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,\
+		field VARCHAR(50) NOT NULL,\
+		value VARCHAR(120),\
+		)"
+	try:
+		cursor = mysql.connect().cursor()
+		cursor.execute(query)
+	except e:
+		raise e
--- runt/trigger.py
+++ runt/trigger.py
@@ -4,7 +4,10 @@ Builds the basics for Runt
 
 """
 import jinja2
+from . import config
 from flask import Flask, render_template
+from flask.ext.mysqldb import MySQL
+#from . import install
 
 trigger = Flask(__name__)
 
@@ -20,6 +23,26 @@ theme_loader = jinja2.ChoiceLoader([
 trigger.jinja_loader = theme_loader
 
 
+mysql = MySQL()
+
+trigger.config.update(config.DB)
+
+mysql.init_app(trigger)
+
 @trigger.route("/admin")
 def admin():
 	return render_template('admin-main.html')
+
+@trigger.route("/install")
+def install():
+
+	query = "CREATE TABLE settings (\
+		id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,\
+		field VARCHAR(50) NOT NULL,\
+		value VARCHAR(120),\
+		)"
+
+	cursor = mysql.connect().cursor()
+	cursor.execute(query)
+
+	return "installed maybe"
