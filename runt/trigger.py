4b041489963028d792cd058cb653f5ff16af1ae2
Garth Reckers <garthreckers@Garths-MacBook-Pro.local>
2016-03-15 18:08:35 -0700

    super basic admin styling, cleaned up some of the url structure, and other stuff

--- runt/trigger.py
+++ runt/trigger.py
@@ -7,7 +7,8 @@ import os
 import jinja2
 from datetime import timedelta
 from . import config
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request,\
+	send_from_directory, redirect, url_for
 from .utils.install import install, check_install
 from .utils.auth import auth, check_username, logged_in
 
@@ -19,7 +20,7 @@ trigger.permanent_session_lifetime = timedelta(hours=3)
 trigger.debug = True
 
 theme_loader = jinja2.ChoiceLoader([
-	    trigger.jinja_loader,	
+		trigger.jinja_loader,	
 		jinja2.FileSystemLoader([
 				'runt/themes',
 				'runt/admin/templates'
@@ -27,16 +28,22 @@ theme_loader = jinja2.ChoiceLoader([
 	])
 trigger.jinja_loader = theme_loader
 
-
-@trigger.route("/admin", methods=['GET', 'POST'])
+@trigger.route("/admin")
 def admin():
+	if not logged_in():
+		return redirect(url_for('admin_login'))
+	return "logged in id: " + logged_in()
+	
+
+@trigger.route("/admin/login", methods=['GET', 'POST'])
+def admin_login():
 
 	err_return = None
 	if request.method == 'POST':
 		if request.form['uname']:
 			if check_username(request.form['uname']):
 				if auth(username=request.form['uname'], password=request.form['password']):
-					return "<h1>BOOM LOGGED IN</h1>"
+					return redirect(url_for('admin'))
 				
 				err_return = "That username and password combination "
 			else:
@@ -45,7 +52,16 @@ def admin():
 			err_return = "Username field is required"
 
 
-	return render_template('admin-main.html', error=err_return)
+	return render_template('admin-login.html', error=err_return)
+
+"""
+Set up static admin css folder
+"""
+@trigger.route('/admin/static/<path:filename>')
+def admin_static(filename):
+	runt_root = os.path.dirname(os.path.realpath(__file__))
+	return send_from_directory(runt_root + '/admin/templates/', filename)
+
 
 @trigger.route("/install", methods=['GET', 'POST'])
 def install_runt():
@@ -85,10 +101,5 @@ def install_runt():
 
 	return render_template('admin-install.html', error=err_return, values=values)
 
-@trigger.route("/admin/lockout")
-def lockout_test():
-	if logged_in():
-		return "logged in id: " + logged_in()
-	return "Not logged in... get out of here"
 
 	
