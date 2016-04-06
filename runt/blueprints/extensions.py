3cae6970d8a192620feece94261f3ee2ad09b171
Garth Reckers <garthreckers@Garths-MacBook-Pro.local>
2016-04-05 22:56:31 -0700

    added on to extension settings and cleaned up settings more

--- runt/blueprints/extensions.py
+++ runt/blueprints/extensions.py
@@ -4,7 +4,7 @@ import sys
 import config
 from flask import Blueprint, send_from_directory,\
 					request, render_template, redirect, url_for
-from runt.models import Settings, Extensions
+from runt.models import Settings
 from runt.extensions import inject_header, inject_footer,\
 								module_install_script
 from ..controllers import ExtensionController
@@ -25,44 +25,7 @@ def admin_extenstions_settings(ext):
 
 @extensions.route("/admin/extensions", methods=['GET', 'POST'], strict_slashes=False)
 def admin_extensions():
-
-	if request.method == 'POST':
-		name = request.form['ext_name']
-		activation = True if request.form['activation'] == 'activate' else False
-
-		select_ext = Extensions.select().where(Extensions.name == name)
-
-		if select_ext.exists():
-			Extensions.update(active=activation).where(Extensions.name == name).execute()
-		else:
-			Extensions.create(name=name, active=activation)
-
-		if activation:
-			module_install_script(name)
-
-		#return redirect(url_for('extensions.admin_install_extensions'))
-
-		
-	exts = Extensions.select().order_by(+Extensions.name)
-
-
-	exts_dict = {}
-	for e in exts:
-		_exts_temp_dict = {}
-		_e_path = config.ROOT_DIR + "/extensions/" + e.name + "/extension.json"
-		if os.path.exists(_e_path):
-			with open(_e_path, "r") as f:
-				_exts_json = json.loads(f.read())['extension_details']
-				_exts_temp_dict.update(_exts_json)
-
-		if e.active == True:
-			_exts_temp_dict.update({"active": True})
-			exts_dict.update({e.name: _exts_temp_dict})
-		else:
-			_exts_temp_dict.update({"active": False})
-			exts_dict.update({e.name: _exts_temp_dict})
-
-	return render_template("extensions.html", pageheader="Extensions", extensions=exts_dict)
+	return e.index()
 
 
 """
--- runt/controllers/extensions.py
+++ runt/controllers/extensions.py
@@ -5,21 +5,65 @@ from ..admin.auth import login_checker, check_username, auth
 from runt.utils import noindex
 from ..admin.install import runt_installed, install_runt
 from flask import render_template, request, redirect, url_for
-from ..models import Settings
+from ..models import Settings, Extensions
 
 class ExtensionController():
 	def __init__(self):
-		pass
+		pass		
+
+	def index(self):
+		if request.method == 'POST':
+			name = request.form['ext_name']
+			activation = True if request.form['activation'] == 'activate' else False
+
+			select_ext = Extensions.select().where(Extensions.name == name)
+
+			if select_ext.exists():
+				Extensions.update(active=activation).where(Extensions.name == name).execute()
+			else:
+				Extensions.create(name=name, active=activation)
+
+			if activation:
+				module_install_script(name)
+
+			#return redirect(url_for('extensions.admin_install_extensions'))
+
+			
+		exts = Extensions.select().order_by(+Extensions.name)
+
+
+		exts_dict = {}
+		for e in exts:
+			_exts_temp_dict = {}
+			_e_path = config.ROOT_DIR + "/extensions/" + e.name + "/extension.json"
+			
+			if os.path.exists(_e_path):
+				with open(_e_path, "r") as f:
+					_exts_json = json.loads(f.read())['extension_details']
+					_exts_temp_dict.update(_exts_json)
+
+			settings_json = config.ROOT_DIR + '/extensions/' + e.name + '/settings.json'
+
+			_exts_temp_dict['has_settings'] = os.path.exists(settings_json)
+
+			if e.active == True:
+				_exts_temp_dict.update({"active": True})
+				exts_dict.update({e.name: _exts_temp_dict})
+			else:
+				_exts_temp_dict.update({"active": False})
+				exts_dict.update({e.name: _exts_temp_dict})
+
+		return render_template("extensions.html", pageheader="Extensions", extensions=exts_dict)
+
 
 	def settings(self, ext_name):
-		"""
-		Need to add settings db read
-		"""
+		settings_json = config.ROOT_DIR + '/extensions/' + ext_name + '/settings.json'
 
-		path_to_json = config.ROOT_DIR + '/extensions/' + ext_name + '/settings.json'
+		if not os.path.exists(settings_json): 
+			return render_template('404.html', pageheader="404 - Page Not Found"), 404
 
 		settings = None
-		with open(path_to_json, "r") as s:
+		with open(settings_json, "r") as s:
 			settings = json.loads(s.read())
 
 		for s in settings:
@@ -43,4 +87,16 @@ class ExtensionController():
 			else:
 				settings[s]['value'] = _init_value
 
-		return render_template("extensions-settings.html", settings=settings, ext_name=ext_name)
+		e_path = config.ROOT_DIR + "/extensions/" + ext_name + "/extension.json"
+			
+		if os.path.exists(e_path):
+			with open(e_path, "r") as f:
+				_exts_json = json.loads(f.read())['extension_details']
+
+		pageheader = _exts_json['name'] or ext_name
+
+		pageheader = pageheader + ' Settings'
+
+
+		return render_template("extensions-settings.html", settings=settings,\
+									ext_name=ext_name, pageheader=pageheader)
